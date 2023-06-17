476f359841bb62345febce167a3d809207a38bb2
univeous <40604180+univeous@users.noreply.github.com>
2023-06-17 12:29:31 +0900

    Update setup.sh

--- spine-godot/build/setup.sh
+++ spine-godot/build/setup.sh
@@ -20,7 +20,9 @@ dev=${2%/}
 pushd ..
 rm -rf godot
 git clone --depth 1 https://github.com/godotengine/godot.git -b $branch
+pushd godot
 git checkout a2575cba4
+pushd ..
 if [ $dev = "true" ]; then
 	cp -r .idea godot
 	cp build/custom.py godot
